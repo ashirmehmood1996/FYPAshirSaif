bdf9d3db43f8df86c2bfc71c1c5ceb386518b950
Ashir <ashirmehmood1996@gmail.com>
2019-04-22 14:36:34 +0500

    resolved the bug of upgraded schema

--- app/src/main/java/com/android/example/fypnotify/dataBase/MembersDatabaseHelper.java
+++ app/src/main/java/com/android/example/fypnotify/dataBase/MembersDatabaseHelper.java
@@ -6,8 +6,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MembersDatabaseHelper extends SQLiteOpenHelper {
     //public static final int DATABASE_VERSION = 1;//older versionm
-    public static final int DATABASE_VERSION = 2;
-    public static final String DATABASE_NAME = "UsersDatabase.db";
+    private static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "UsersDatabase.db";
 
     public MembersDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -28,6 +28,7 @@ public class MembersDatabaseHelper extends SQLiteOpenHelper {
                 + DatabaseContract.NotificationsEntry.COLOUMN_TITLE + " TEXT NOT NULL DEFAULT 'untitled', "
                 + DatabaseContract.NotificationsEntry.COLOUMN_MESSAGE + " TEXT NOT NULL,"
                 + DatabaseContract.NotificationsEntry.COLOUMN_TIME_STAMP + " TEXT NOT NULL DEFAULT 'untitled',"
+                + DatabaseContract.NotificationsEntry.COLOUMN_URI_LIST + " TEXT DEFAULT NULL,"
                 + DatabaseContract.NotificationsEntry.COLOUMN_RECIEVERS + " TEXT NOT NULL );";
         sqLiteDatabase.execSQL(CREATE_MEMBERS_TABLE);
         sqLiteDatabase.execSQL(CREATE_NOTIFICATION_TABLE);
@@ -35,10 +36,10 @@ public class MembersDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        String ALTER_NOTIFICATION_TABLE = "ALTER TABLE " + DatabaseContract.NotificationsEntry.TABLE_NAME
-                + " ADD COLUMN " + DatabaseContract.NotificationsEntry.COLOUMN_URI_LIST + " TEXT DEFAULT NULL;";
-
-        sqLiteDatabase.execSQL(ALTER_NOTIFICATION_TABLE);
+//        String ALTER_NOTIFICATION_TABLE = "ALTER TABLE " + DatabaseContract.NotificationsEntry.TABLE_NAME
+//                + " ADD COLUMN " + DatabaseContract.NotificationsEntry.COLOUMN_URI_LIST + " TEXT DEFAULT NULL;";
+//
+//        sqLiteDatabase.execSQL(ALTER_NOTIFICATION_TABLE);
         //when we add new table or add new coloum to an existing table or modify schema in any form then we need to use on upgrade mehod for further deatails follow th elink below
         //https://stackoverflow.com/questions/24634116/caused-by-android-database-sqlite-sqliteexception-no-such-table-code-1-andr
     }
